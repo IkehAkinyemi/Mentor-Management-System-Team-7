7b864fa4f882d6d2870b8a17d0e538b2e583e250
ido evergeen <dicksonevegreenido@gmail.com>
2023-04-29 16:11:23 +0100

    add FAQ questions

--- frontend/mms-Admin/pages/admin/settings/faq.tsx
+++ frontend/mms-Admin/pages/admin/settings/faq.tsx
@@ -2,15 +2,34 @@ import SettingsLayout from "@/components/Settings/SettingsLayout";
 import { DashboardLayout } from "@/components/layouts/dashboard-layout";
 import React, { ReactElement } from "react";
 import { useFetchFAQ } from "../../../hooks/useFAQ";
-
+import { ClipLoader, RotateLoader } from "react-spinners";
+import Accordion from "@/components/Accordion";
 
 const FAQ = () => {
+  const { data, error, isLoading } = useFetchFAQ();
 
+  console.log(data?.data, "FAQ");
 
-  const { data , error } = useFetchFAQ()
+  if (isLoading) {
+    return (
+      <div className="mx-auto flex justify-center">
+        {/* <RotateLoader color="#000" loading={isLoading} size={15} /> */}
+        <ClipLoader color="#36d7b7" />
+      </div>
+    );
+  }
+  return (
+    <div className="border border-[#E6E6E6] rounded-md p-3">
 
-  console.log(data , 'FAQ')
-  return <div>FAQ</div>;
+      <h1 className="text-xl font-bold text-mmsBlack2 mb-[28px]">
+      General FAQ
+      </h1>
+
+      {data?.data.data.map((item: any) => (
+        <Accordion question={item.question} answer={item.answer} />
+      ))}
+    </div>
+  );
 };
 
 export default FAQ;
@@ -24,4 +43,3 @@ FAQ.getLayout = function getLayout(page: ReactElement) {
 };
 
 FAQ.requireAuth = true;
-
